36e56a4cf8f8d3c3df3c13cd0a98615e38018d58
qiaoyu <qiaoyu1002@gmail.com>
2023-06-29 11:28:02 +0000

    add MobileSAM

--- ultralytics/vit/__init__.py
+++ ultralytics/vit/__init__.py
@@ -2,5 +2,6 @@
 
 from .rtdetr import RTDETR
 from .sam import SAM
+from .mobilesam import MobileSAM
 
-__all__ = 'RTDETR', 'SAM'  # allow simpler import
+__all__ = 'RTDETR', 'SAM' , 'MobileSAM' # allow simpler import
